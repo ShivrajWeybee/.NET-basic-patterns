73e51d261fae4152384b4d34b7e0d6e629af133c
Shivraj Chvada <shivraj@webtheunivesre.com>
2023-03-13 11:42:58 +0530

    initial commit

--- BasicPattern/Program.cs
+++ BasicPattern/Program.cs
@@ -8,60 +8,41 @@ namespace BasicPattern
 {
     class Program
     {
+        public enum Days { Sun, Mon, Tue, Wed, Thu, Fri, Sat };
+        public enum Seasons { WINTER, SPRING, SUMMER, FALL }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a number of rows");
-            int n = int.Parse(Console.ReadLine());
-            Console.WriteLine();
+            //Game game1 = new Game("Super Mario", "Nintendo", 4.5, 1985, 110);
+            //Game game2 = new Game("Halo", "343 Industries", 4.0, 2000, 2000);
 
-            // keep track of rows
-            for (int i = 0; i < n; i++)
-            {
-                // first half
-                for (int j = 0; j < i; j++)
-                {
-                    Console.Write(Convert.ToChar((j + 65)) + " ");
-                }
+            //game2.rating = 4.5;
 
-                // second half
-                for (int j = i; j >= 0; j--)
-                {
-                    Console.Write(Convert.ToChar((j + 65)) + " ");
-                }
-                Console.WriteLine();
-            }
+            //game2.increamentUser(game2.title);
 
-            Console.WriteLine();
+            //game1 = null; // make object null so that garbage collector can manage this
+            //GC.Collect(); // Explicitly calling Garbage Collector
+
+            //Console.WriteLine(MathOperation.addition(15, 15));
+
+            //int x = (int)Seasons.WINTER;
+            //int y = (int)Seasons.SUMMER;
+
+            //Console.WriteLine(x + " " + y);
 
 
-            // -------------------------------------------------------------------------
 
-            // Palindrome Pyramid
+            Chef chef1 = new Chef();
+            chef1.MakeSpecialDish();
 
-            // keep track of rows
-            for (int i = 0; i < n; i++)
-            {
-                // spaces
-                for(int j = 1; j <= (n - i); j++)
-                {
-                    Console.Write("  ");
-                }
-
-                // first half
-                for (int j = 0; j < i; j++)
-                {
-                    Console.Write(Convert.ToChar((j + 65)) + " ");
-                }
-
-                // second half
-                for (int j = i; j >= 0; j--)
-                {
-                    Console.Write(Convert.ToChar((j + 65)) + " ");
-                }
-                Console.WriteLine();
-            }
+            ItalianChef chef2 = new ItalianChef();
+            chef2.MakeSpecialDish();
 
             Console.ReadLine();
+        }
+        static Program()
+        {
+            Console.WriteLine("Static ...");
         }
     }
 }
