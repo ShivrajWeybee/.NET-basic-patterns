9b4ccd00084613c0d6913a072a89d28ad30f4526
Shivraj Chvada <shivraj@webtheunivesre.com>
2023-03-14 11:37:20 +0530

    try,catch,finally & custom

--- Learning/Program.cs
+++ Learning/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,101 +7,60 @@ using System.Threading.Tasks;
 
 namespace BasicPattern
 {
+    public class CustomException : Exception
+    {
+        public CustomException()
+            : base()
+        {
+        }
+
+        public CustomException(string message)
+            : base(message)
+        {
+        }
+
+        public CustomException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            // simple array
-            int[] simpleArr = new int[3] { 1, 2, 3 };
-            int[] simpleArr2 = { 1, 2, 3, 4 };
+            StreamReader streamReader = null;
 
-            for (int i = 0; i < simpleArr.Length; i++)
+            try
             {
-                Console.WriteLine(simpleArr[i]);
+                streamReader = new StreamReader(@"C:\abc.txt");
+                Console.WriteLine(streamReader.ReadToEnd());
+                throw new CustomException("This is custom exception");
             }
-
-            // multiDimension array
-            string[,] multiDimensionArr = new string[2, 2] { { "a", "A" }, { "b", "B" } };
-
-            foreach (string i in multiDimensionArr)
+            catch (FileNotFoundException fex)
             {
-                Console.WriteLine(i);
+                Console.WriteLine("please check that file {0} exists", fex.FileName);
             }
-
-            for (int i = 0; i < multiDimensionArr.GetLength(0); i++)
+            catch(DirectoryNotFoundException dex)
             {
-                for (int j = 0; j < multiDimensionArr.GetLength(1); j++)
+                Console.WriteLine("Please check that this {0} path is correct", dex.Data);
+            }
+            catch(CustomException cex)
+            {
+                Console.WriteLine(cex.Message);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if(streamReader != null)
                 {
-                    Console.WriteLine(multiDimensionArr[i, j]);
+                    streamReader.Close();
                 }
             }
-
-            // jagged array
-            int[][] jaggedArr = new int[2][] { new int[] { 1, 2, 3 }, new int[] { 11, 22 } };
-            //jaggedArr[2] = new int[] { 111, 222, 333 };
-
-            for (int i = 0; i < jaggedArr.Length; i++)
-            {
-                for (int j = 0; j < jaggedArr[i].Length; j++)
-                {
-                    Console.Write(jaggedArr[i][j] + " ");
-                }
-                Console.WriteLine();
-            }
-
-
-            // --------------------------------------------------------------
-
-            // switch case
-            Console.WriteLine("Enter no. from 1-3");
-            int n = int.Parse(Console.ReadLine());
-
-            switch (n)
-            {
-                case 1:
-                    Console.WriteLine("Entered 1");
-                    break;
-
-                case 2:
-                    Console.WriteLine("Entered 2");
-                    break;
-
-                case 3:
-                    Console.WriteLine("Entered 3");
-                    break;
-
-                default:
-                    Console.WriteLine("Invalid input");
-                    break;
-            }
-
-
-            // --------------------------------------------------------------
-
-            // ref vs out
-            int resultRef = 0;
-            Console.WriteLine("before {0}", resultRef);
-            AddNumber(1, 1, ref resultRef);
-            Console.WriteLine("after {0}", resultRef);
-
-            int resultOut;
-            //Console.WriteLine("before {0}", resultOut);
-            AddNumberOut(1, 1, out resultOut);
-            Console.WriteLine("after {0}", resultOut);
 
             Console.ReadLine();
         }
-
-        public static int AddNumber(int n1, int n2, ref int number)
-        {
-            number = n1 + n2;
-            return number;
-        }
-        public static int AddNumberOut(int n1, int n2, out int number)
-        {
-            number = n1 + n2;
-            return number;
-        }
-
     }
 }
